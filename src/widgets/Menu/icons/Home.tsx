5db3ef113d56ed7dc5cc17b56f7abdb70726836c
Nnamdi Arimah <nnamdi.arimah@shopify.com>
2021-11-11 18:56:48 -0500

    Resize icon strokeWidth

--- src/widgets/Menu/icons/Home.tsx
+++ src/widgets/Menu/icons/Home.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../../../components/Svg/types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 355 307" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path fill="#101820" stroke="#b5bd00" strokeMiterlimit="10" strokeWidth="3px" d="M364.75,177.57h0L203.36,36.74h0a16,16,0,0,0-22.72,0h0L19.25,177.57h0A8,8,0,0,0,24,192H64V320a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V224h64v96a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V192h40a8,8,0,0,0,4.75-14.43Z" transform="translate(-14.5 -30.5)"/>
+      <path fill="#101820" stroke="#b5bd00" strokeMiterlimit="10" strokeWidth="10px" d="M364.75,177.57h0L203.36,36.74h0a16,16,0,0,0-22.72,0h0L19.25,177.57h0A8,8,0,0,0,24,192H64V320a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V224h64v96a16,16,0,0,0,16,16h64a16,16,0,0,0,16-16V192h40a8,8,0,0,0,4.75-14.43Z" transform="translate(-14.5 -30.5)"/>
     </Svg>
   );
 };
--- src/widgets/Menu/icons/More.tsx
+++ src/widgets/Menu/icons/More.tsx
@@ -5,9 +5,9 @@ import { SvgProps } from "../../../components/Svg/types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 72.75 18.75" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <circle fill="#101820" stroke="#b5bd00" strokeMiterlimit="10" strokeWidth="0.75px" cx="9.38" cy="9.38" r="9" />
-      <circle fill="#101820" stroke="#b5bd00" strokeMiterlimit="10" strokeWidth="0.75px" className="cls-1" cx="36.38" cy="9.38" r="9" />
-      <circle fill="#101820" stroke="#b5bd00" strokeMiterlimit="10" strokeWidth="0.75px" className="cls-1" cx="63.38" cy="9.38" r="9" />
+      <circle fill="#101820" stroke="#b5bd00" strokeMiterlimit="0" strokeWidth="2px" cx="9.38" cy="9.38" r="9" />
+      <circle fill="#101820" stroke="#b5bd00" strokeMiterlimit="10" strokeWidth="2px" className="cls-1" cx="36.38" cy="9.38" r="9" />
+      <circle fill="#101820" stroke="#b5bd00" strokeMiterlimit="10" strokeWidth="2px" className="cls-1" cx="63.38" cy="9.38" r="9" />
     </Svg>
   );
 };
